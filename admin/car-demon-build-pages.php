883bc8af0ae669413968bc59db1fcf7105afc5d9
theverylastperson <theverylastperson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-24 15:18:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/car-demon/trunk@616863 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/car-demon-build-pages.php
@@ -0,0 +1,38 @@
+<?php
+function car_demon_add_page($title, $type) {
+	if ($type == 'contact') {
+		$content = '[-contact_us-]';
+	}
+	elseif ($type == 'finance') {
+		$content = '[-finance_form-]';
+	}
+	elseif ($type == 'service_appointment') {
+		$content = '[-service_form-]';
+	}
+	elseif ($type == 'service_quote') {
+		$content = '[-service_quote-]';
+	}
+	elseif ($type == 'parts') {
+		$content = '[-part_request-]';
+	}
+	elseif ($type == 'trade') {
+		$content = '[-trade-]';
+	}
+	elseif ($type == 'staff') {
+		$content = '[-staff-]';
+	}
+	$current_user = wp_get_current_user();
+	$user_id = $current_user->ID;
+	$my_page = array(
+		'post_title'    => $title,
+		'post_content'  => $content,
+		'post_status'   => 'publish',
+		'post_author'   => $user_id,
+		'post_type'     => 'page',
+		'comment_status'=> 'closed'
+	);
+	// Insert the post into the database
+	$post_id = wp_insert_post( $my_page );
+	return $post_id;
+}
+?>
---
+++ admin/car-demon-columns.php
@@ -0,0 +1,206 @@
+<?php
+add_action('manage_cars_for_sale_posts_custom_column', 'manage_cars_for_sale_columns');
+add_filter("manage_edit-cars_for_sale_columns", "my_cars_for_sale_columns");
+add_action('quick_edit_custom_box',  'max_add_quick_edit', 10, 2);
+
+function my_cars_for_sale_columns($columns) {
+	$columns = array_merge(array(
+		"cb" => "<input type=\"checkbox\" />",
+		"title" => "Vehicle Title",
+		"photo" => "Photo",
+		"msrp" => "Retail Price",
+		"rebate" => "Rebate",
+		"discount" => "Discount",
+		"price" => 'Price',
+		"sold" => 'Sold'
+	),$columns);
+	return $columns;
+}
+
+function manage_cars_for_sale_columns($column) {
+	global $post;
+	$post_id = $post->ID;
+	$post_title = $post->post_title;
+	$car_demon_pluginpath = str_replace(str_replace('\\', '/', ABSPATH), get_option('siteurl').'/', str_replace('\\', '/', dirname(__FILE__))).'/';
+	$car_demon_pluginpath = str_replace('admin','',$car_demon_pluginpath);
+	$car_pic = wp_get_attachment_thumb_url( get_my_post_thumbnail_id_detail_eil( $post_id ) );
+	$car_pic = str_replace('-150x150', '', $car_pic);
+	if (empty($car_pic)) {
+		$car_demon_pluginpath = str_replace(str_replace('\\', '/', ABSPATH), get_option('siteurl').'/', str_replace('\\', '/', dirname(__FILE__))).'/';
+		$car_demon_pluginpath = str_replace('admin','',$car_demon_pluginpath);
+		$car_pic = $car_demon_pluginpath.'images/no_photo.gif';
+	}
+	$attachments = get_children( array( 'post_parent' => $post_id ) );
+	$count = count( $attachments );
+	$car_image = '<img width="75" src="'.$car_pic.'" /><br />('.$count.')';
+	if ("ID" == $column) {
+		echo $post_id;
+	}
+	elseif ("photo" == $column) {
+		echo $car_image;
+	}
+	elseif ("msrp" == $column) {
+		echo '<input type="text" id="msrp_'.$post_id.'" onchange="update_car('.$post_id.', this, \'_msrp_value\');" size="6" value="'.get_post_meta($post_id, '_msrp_value', true).'" />';
+	}
+	elseif ("rebate" == $column) {
+		echo  '<input type="text" id="rebate_'.$post_id.'" onchange="update_car('.$post_id.', this, \'_rebates_value\');" size="6" value="'.get_post_meta($post_id, '_rebates_value', true).'" />';
+	}
+	elseif ("discount" == $column) {
+		echo '<input type="text" id="discount_'.$post_id.'" onchange="update_car('.$post_id.', this, \'_discount_value\');" size="6" value="'.get_post_meta($post_id, '_discount_value', true).'" />';
+	}
+	elseif ("price" == $column) {
+		$price_color = '';
+		$msrp = get_post_meta($post_id, '_msrp_value', true);
+		$rebates = get_post_meta($post_id, '_rebates_value', true);
+		$discounts = get_post_meta($post_id, '_discount_value', true);
+		if (empty($msrp)) {$msrp = 0;}
+		if (empty($rebates)) {$rebates = 0;}
+		if (empty($discounts)) {$discounts = 0;}
+		$calculated_price = $msrp - $rebates - $discounts;
+		$final_price = get_post_meta($post_id, '_price_value', true);
+		$calculated_discount = $final_price - $msrp;
+		echo 'Calculated Price: <span id="calc_price_'.$post_id.'">'.$calculated_price.'</span><br />';
+		echo 'Calculated Discount: <span id="calc_discounts_'.$post_id.'">'.$calculated_discount.'</span><br />';
+		echo '<span id="calc_error_'.$post_id.'"></span>';
+		if ($calculated_price != $final_price) {
+			if (!empty($msrp)) {
+				$price_color = ' style="background-color:#FFB3B3;"';
+			}
+		}
+		echo '<input'.$price_color.' id="price_'.$post_id.'" type="text" onchange="update_car('.$post_id.', this, \'_price_value\');" size="6" value="'.$final_price.'" />';
+	}
+	elseif ("sold" == $column) {
+		$sold = get_post_meta($post_id, 'sold', true);
+		echo '<div id="show_sold_'.$post_id.'">
+				<select id="is_sold_'.$post_id.'" onchange="update_car_sold('.$post_id.', this, \'sold\')">
+					<option value="'.$sold.'">'.$sold.'</option>
+					<option value="Yes">Yes</option>
+					<option value="No">No</option>
+				</select>
+			</div>';
+	}
+}
+
+function max_add_quick_edit($column_name, $post_type) {
+	if ($column_name == 'msrp') {
+		echo '<table><tr><td>';
+		echo '<fieldset class="inline-edit-col-left">';
+		echo '<div class="inline-edit-col">';
+		echo '<span class="title">MSRP</span>';
+		echo '<input type="text" name="msrp_val" id="msrp_val" />';
+		echo '</div>';
+		echo '</fieldset><br />';
+	}
+	if ($column_name == 'rebate') {
+		echo '<fieldset class="inline-edit-col-left">';
+		echo '<div class="inline-edit-col">';
+		echo '<span class="title">Rebates</span>';
+		echo '<input type="text" name="rebates_val" id="rebates_val" />';
+		echo '</div>';
+		echo '</fieldset><br />';
+	}
+	if ($column_name == 'discount') {
+		echo '<fieldset class="inline-edit-col-left">';
+		echo '<div class="inline-edit-col">';
+		echo '<span class="title">Discount</span>';
+		echo '<input type="text" name="discount_val" id="discount_val" />';
+		echo '</div>';
+		echo '</fieldset><br />';
+	}
+	if ($column_name == 'price') {
+		echo '<fieldset class="inline-edit-col-left">';
+		echo '<div class="inline-edit-col">';
+		echo '<span class="title">Price</span>';
+		echo '<input type="text" name="price_val" id="price_val" />';
+		echo '</div>';
+		echo '</fieldset><br />';
+		echo '</td></tr></table>';
+	}
+	return;
+}
+
+function max_save_quick_edit_data($post_id) {
+	// verify if this is an auto save routine. If it is our form has not been submitted, so we dont want
+	// to do anything
+	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+		return $post_id;	
+	// Check permissions
+	if (isset($_POST['post_type'])) {
+		$post_type = $_POST['post_type'];
+	}
+	else {
+		$post_type = '';
+	}
+	if ( 'page' == $post_type ) {
+		if ( !current_user_can( 'edit_page', $post_id ) )
+			return $post_id;
+	} else {
+		if ( !current_user_can( 'edit_post', $post_id ) )
+		return $post_id;
+	}	
+	// OK, we're authenticated: we need to find and save the data
+	$post = get_post($post_id);
+	if (isset($_POST['msrp_val']) && ($post->post_type != 'revision')) {
+		$msrp_val = esc_attr($_POST['msrp_val']);
+		if ($msrp_val)
+			update_post_meta( $post_id, '_msrp_value', $msrp_val);		
+		else
+			delete_post_meta( $post_id, '_msrp_value');		
+	}
+	if (isset($_POST['rebates_val']) && ($post->post_type != 'revision')) {
+		$rebates_val = esc_attr($_POST['rebates_val']);
+		if ($rebates_val)
+			update_post_meta( $post_id, '_rebates_value', $rebates_val);
+		else
+			delete_post_meta( $post_id, '_rebates_value');		
+	}
+	if (isset($_POST['discount_val']) && ($post->post_type != 'revision')) {
+		$discount_val = esc_attr($_POST['discount_val']);
+		if ($discount_val)
+			update_post_meta( $post_id, '_discount_value', $discount_val);		
+		else
+			delete_post_meta( $post_id, '_discount_value');		
+	}
+	if (isset($_POST['price_val']) && ($post->post_type != 'revision')) {
+		$price_val = esc_attr($_POST['price_val']);
+		if ($price_val)
+			update_post_meta( $post_id, '_price_value', $price_val);		
+		else
+			delete_post_meta( $post_id, '_price_value');		
+	}
+	return $post_id;
+}
+
+function max_quick_edit_javascript() {
+	global $current_screen;
+	?>
+	<script type="text/javascript">
+	<!--
+	function set_msrp(msrp_val,rebates,discount,price) {
+		// revert Quick Edit menu so that it refreshes properly
+		inlineEditPost.revert();
+		document.getElementById('msrp_val').value = msrp_val;
+		document.getElementById('rebates_val').value = rebates;
+		document.getElementById('discount_val').value = discount;
+		document.getElementById('price_val').value = price;
+	}
+	//-->
+	</script>
+	<?php
+}
+
+function max_expand_quick_edit_link($actions, $post) {
+	global $current_screen;
+	$nonce = wp_create_nonce( 'msrp'.$post->ID);
+	$msrp = get_post_meta( $post->ID, '_msrp_value', TRUE);
+	$rebates = get_post_meta( $post->ID, '_rebates_value', TRUE);
+	$discount = get_post_meta( $post->ID, '_discount_value', TRUE);
+	$price = get_post_meta( $post->ID, '_price_value', TRUE);
+	$actions['inline hide-if-no-js'] = '<a href="#" class="editinline" title="';
+	$actions['inline hide-if-no-js'] .= esc_attr( __( 'Edit this item inline','car-demon' ) ) . '" ';
+	$actions['inline hide-if-no-js'] .= " onclick=\"set_msrp('$msrp','$rebates','$discount','$price')\">"; 
+	$actions['inline hide-if-no-js'] .= __( 'Quick&nbsp;Edits','car-demon' );
+	$actions['inline hide-if-no-js'] .= '</a>';
+	return $actions;	
+}
+?>
