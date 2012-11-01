6ab924fbe66512c5890131a9d2cb0d5404623209
theverylastperson <theverylastperson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-01 21:16:03 +0000

    updating to tag 1.0.9
    git-svn-id: https://plugins.svn.wordpress.org/car-demon/trunk@619993 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- car-demon.php
+++ car-demon.php
@@ -4,7 +4,7 @@ Plugin Name: Car Demon
 Plugin URI: http://www.CarDemons.com/
 Description:  Car Demon is a PlugIn designed for car dealers.
 Author: CarDemons
-Version: 1.0.8
+Version: 1.0.9
 Author URI: http://www.CarDemons.com/
 */
 
