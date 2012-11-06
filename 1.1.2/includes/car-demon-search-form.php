33264f5825b99bc4439a7ffdce5d3b66e2837a1b
theverylastperson <theverylastperson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-06 21:20:19 +0000

    moving to 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/car-demon/tags@621823 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.1.2/includes/car-demon-search-form.php
@@ -0,0 +1,199 @@
+<?php
+function car_demon_search_form() {
+	$car_demon_pluginpath = str_replace(str_replace('\\', '/', ABSPATH), get_option('siteurl').'/', str_replace('\\', '/', dirname(__FILE__))).'/';
+	$car_demon_pluginpath = str_replace('includes','',$car_demon_pluginpath);
+	car_demon_search_cars_scripts();
+	$url = get_option('siteurl');
+?>
+<div class="search_car_box_frame">
+	<div id="car-demon-search-cars" class="search_car_box">
+<form action="<?php echo $url ?>" method="get" />
+<input type="hidden" name="s" value="cars" />
+<input type="hidden" name="car" value="1" />
+		<div id="car-demon-searchr1c1" class="search_header"></div>
+		<div id="car-demon-searchr2c1" class="search_header_logo"><img src="<?php echo $car_demon_pluginpath; ?>theme-files/images/search_cars.png" alt="Search Cars" title="Search Cars" />&nbsp;QUICK SEARCH
+<?php
+	echo '&nbsp;<span class="advanced_search_btn" onclick="document.getElementById(\'advanced_search\').style.display=\'inline\';" title="Advanced Search">+</span></div>';
+	echo '<div class="advanced_search" id="advanced_search">Stock #: <input class="search_dropdown_sm" type="text" name="stock" id="stock" size="6" />
+	&nbsp;<span class="advanced_search_btn_hide" onclick="document.getElementById(\'advanced_search\').style.display=\'none\';" title="Hide Advanced Search">-</span>
+	</div>';
+?>
+		<div>
+			<div class="search_left">
+				<div id="car-demon-searchr3c1" class=""><?php _e('Condition','car-demon'); ?>:</div>
+				<div><?php echo car_demon_search_condition(); ?></div>
+			</div>
+			<div class="search_right">
+				<div id="car-demon-searchr3c1" class=""><?php _e('Manufacturer','car-demon'); ?>:</div>
+				<div id="car-demon-searchr4c1" class=""><?php echo car_demon_search_makes();?></div>
+			</div>
+		</div>
+		<div>
+			<div class="search_left">
+				<div class="search_labels"><?php _e('Year','car-demon'); ?>:</div>
+				<div><?php echo car_demon_search_years(); ?></div>
+			</div>
+			<div class="search_right">
+				<div class=""><?php _e('Model','car-demon'); ?>:</div>
+				<div><?php echo car_demon_search_models();?></div>
+			</div>
+		</div>
+		<div>
+			<div id="car-demon-searchr6c1" class="search_min_price">
+				<div class="search_labels"><?php _e('Min Price','car-demon'); ?>:</div>
+				<div>
+					<?php echo car_demon_search_price('Min'); ?>
+				</div>
+			</div>
+			<div id="car-demon-searchr6c2" class="search_max_price">
+				<div class="search_labels"><?php _e('Max Price','car-demon'); ?>:</div>
+				<div>
+					<?php echo car_demon_search_price('Max'); ?>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div id="car-demon-searchr7c1" class="search_trans">
+				<div class="search_labels"><?php _e('Trans','car-demon'); ?>:</div>
+				<div>
+					<?php echo car_demon_search_tran(); ?>
+				</div>
+			</div>
+			<div id="car-demon-searchr7c2" class="search_mileage">
+				<div class="search_labels"><?php _e('Mileage','car-demon'); ?>:</div>
+				<div>
+					<?php echo car_demon_search_miles(); ?>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div id="car-demon-searchr8c1" class="search_body">
+				<div class="search_labels"><?php _e('Body Type','car-demon'); ?>:</div>
+				<div>
+					<?php echo car_demon_search_body(); ?>
+				</div>
+			</div>
+			<div id="car-demon-searchr8c2" class="search_button_box">
+			  <input type="submit" name="submit_search" id="submit_search" value="<?php _e('Search','car-demon'); ?>" class="search_btn">
+			</div>
+		</div>
+		<div id="car-demon-searchr9c1" class="search_footer"></div>
+</form>
+	</div>
+</div>
+<?php
+}
+
+function car_demon_simple_search($size='l') {
+	$car_demon_auto_credit_themepath = str_replace(str_replace('\\', '/', ABSPATH), $url = get_option('siteurl').'/', str_replace('\\', '/', dirname(__FILE__))).'/';
+	$car_demon_auto_credit_themepath = str_replace('includes','',$car_demon_auto_credit_themepath);
+	$car_demon_themepath = str_replace('car-demon-auto-credit','car-demon', $car_demon_auto_credit_themepath);
+	car_demon_search_cars_scripts();
+	$url = get_option('siteurl');
+	if ($size == 's') {
+		$form_size = "search_car_box_frame_narrow";
+		$logo = "search_header_logo_simple";
+	}
+	else {
+		$form_size = "search_car_box_frame_wide";
+		$logo = "search_header_logo_simple_wide";
+	}
+?>
+<div class="<?php echo $form_size; ?>">
+	<div id="car-demon-search-cars_sm" class="search_car_box_sm">
+<form action="<?php echo $url ?>" method="get" />
+<input type="hidden" name="s" value="cars" />
+<input type="hidden" name="car" value="1" />
+		<div id="car-demon-searchr2c1" class="<?php echo $logo; ?>"><img src="<?php echo $car_demon_themepath; ?>theme-files/images/search_cars.png" alt="Search Cars" title="Search Cars" />&nbsp;<?php _e('QUICK SEARCH','car-demon'); ?></div>
+		<div class="search_left">
+			<div id="car-demon-searchr3c1" class="search_manufacturer_title2"><?php _e('Year','car-demon'); ?>:</div>
+			<div><?php echo car_demon_search_years(); ?></div>
+		</div>
+		<div class="search_right">
+			<div id="car-demon-searchr3c1" class=""><?php _e('Manufacturer','car-demon'); ?>:</div>
+			<div id="car-demon-searchr4c1" class=""><?php echo car_demon_search_makes();?></div>
+		</div>
+		<div class="search_right">
+			<div id="car-demon-searchr3c1" class=""><?php _e('Model','car-demon'); ?>:</div>
+			<div id="car-demon-searchr5c1" class=""><?php echo car_demon_search_models();?></div>
+		</div>
+		<div id="car-demon-searchr8c1" class="search_body"><?php _e('Body Type','car-demon'); ?>:<br /><?php echo car_demon_search_body(); ?></div>
+		<div id="car-demon-searchr8c2" class="search_button_box">
+		  <input type="submit" name="submit_search" id="submit_search" value="<?php _e('Search','car-demon'); ?>" class="search_btn">
+		</div>
+</form>
+	</div>
+</div>
+<?php
+}
+
+function car_demon_get_searched_by() {
+	$searched = '';
+	$query_string = $_SERVER['QUERY_STRING'];
+	$query_string = str_replace('%2C', ',', $query_string);
+	if (isset($_GET['search_condition'])) {
+		if ($_GET['search_condition']) {
+			$searched .= '<span class="remove_search" onclick="remove_search(\'search_condition\', \''.$_GET['search_condition'].'\', \''.$query_string.'\');">x</span> <span class="remove_search_title">Condition:</span> ';
+			$searched .= $_GET['search_condition'] .', ';
+		}
+	}
+	if (isset($_GET['search_year'])) {
+		if ($_GET['search_year']) {
+			$searched .= '<span class="remove_search" onclick="remove_search(\'search_year\', \''.$_GET['search_year'].'\', \''.$query_string.'\');">x</span> <span class="remove_search_title">Year:</span> ';
+			$searched .= $_GET['search_year'];
+		}
+	}
+	if (isset($_GET['search_make'])) {
+		if ($_GET['search_make']) {
+			$searched .= '<span class="remove_search" onclick="remove_search(\'search_make\', \''.$_GET['search_make'].'\', \''.$query_string.'\');">x</span> <span class="remove_search_title">Make:</span> ';
+			$search_make_array = $_GET['search_make'];
+			$search_make_array = explode(',', $search_make_array);
+			$search_make = $search_make_array[1];
+			$searched .= $search_make .', ';
+		}
+	}
+	if (isset($_GET['search_model'])) {
+		if ($_GET['search_model']) {
+			$searched .= '<span class="remove_search" onclick="remove_search(\'search_model\', \''.$_GET['search_model'].'\', \''.$query_string.'\');">x</span> <span class="remove_search_title">Model:</span> ';
+			$searched .= $_GET['search_model'] .', ';
+		}
+	}
+	if (isset($_GET['search_dropdown_Min_price'])) {
+		if ($_GET['search_dropdown_Min_price']) {
+			$searched .= '<span class="remove_search" onclick="remove_search(\'search_dropdown_Min_price\', \''.$_GET['search_dropdown_Min_price'].'\', \''.$query_string.'\');">x</span> <span class="remove_search_title">Min Price:</span> ';
+			$searched .= $_GET['search_dropdown_Min_price'] .', ';
+		}
+	}
+	if (isset($_GET['search_dropdown_Max_price'])) {
+		if ($_GET['search_dropdown_Max_price']) {
+			$searched .= '<span class="remove_search" onclick="remove_search(\'search_dropdown_Max_price\', \''.$_GET['search_dropdown_Max_price'].'\', \''.$query_string.'\');">x</span> <span class="remove_search_title">Max Price:</span> ';
+			$searched .= $_GET['search_dropdown_Max_price'] .', ';
+		}
+	}
+	if (isset($_GET['search_dropdown_tran'])) {
+		if ($_GET['search_dropdown_tran']) {
+			$searched .= '<span class="remove_search" onclick="remove_search(\'search_dropdown_tran\', \''.$_GET['search_dropdown_tran'].'\', \''.$query_string.'\');">x</span> <span class="remove_search_title">Transmission:</span> ';
+			$searched .= $_GET['search_dropdown_tran'] .', ';
+		}
+	}
+	if (isset($_GET['search_dropdown_miles'])) {
+		if ($_GET['search_dropdown_miles']) {
+			$searched .= '<span class="remove_search" onclick="remove_search(\'search_dropdown_miles\', \''.$_GET['search_dropdown_miles'].'\', \''.$query_string.'\');">x</span> <span class="remove_search_title">Miles:</span> ';
+			$searched .= $_GET['search_dropdown_miles'] .', ';
+		}
+	}
+	if (isset($_GET['search_dropdown_body'])) {
+		if ($_GET['search_dropdown_body']) {
+			$searched .= '<span class="remove_search" onclick="remove_search(\'search_dropdown_body\', \''.$_GET['search_dropdown_body'].'\', \''.$query_string.'\');">x</span> <span class="remove_search_title">Body Style:</span> ';
+			$searched .= $_GET['search_dropdown_body'] .', ';
+		}
+	}
+	$searched .= '@@';
+	$searched = str_replace(', @@', '', $searched);
+	$searched = str_replace('@@', '', $searched);
+	if (!empty($searched)) {
+		$searched = '<div class="searched_by">'.$searched.'</div>';
+	}
+	return $searched;
+}
+?>
---
+++ 1.1.2/widgets/car-demon-random-cars.php
@@ -0,0 +1,169 @@
+<?php
+add_action( 'widgets_init', 'car_demon_random_car_load_widgets' );
+
+function car_demon_random_car_load_widgets() {
+	register_widget( 'car_demon_random_car_Widget' );
+}
+
+class car_demon_random_car_Widget extends WP_Widget {
+	/**
+	 * Widget setup.
+	 */
+	function car_demon_random_car_Widget() {
+		/* Widget settings. */
+		$widget_ops = array( 'classname' => 'car_demon_random_car', 'description' => __('Display random cars.', 'car-demon') );
+
+		/* Widget control settings. */
+		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'car_demon_random_car-widget' );
+
+		/* Create the widget. */
+		$this->WP_Widget( 'car_demon_random_car-widget', __('Car Demon Random Cars', 'car-demon'), $widget_ops, $control_ops );
+	}
+	/**
+	 * How to display the widget on the screen.
+	 */
+	function widget( $args, $instance ) {
+		extract( $args );
+		/* Our variables from the widget settings. */
+		$title = apply_filters('widget_title', $instance['title'] );
+		$cars = $instance['cars'];
+
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+
+		/* Display the widget title if one was input (before and after defined by themes). */
+		if (!empty($title)) {
+			echo $before_title . $title . $after_title;
+		}
+		echo car_demon_display_random_cars($cars);
+		/* After widget (defined by themes). */
+		echo $after_widget;
+	}
+	/**
+	 * Update the widget settings.
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		/* Strip tags for title and name to remove HTML (important for text inputs). */
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['cars'] = strip_tags( $new_instance['cars'] );
+		return $instance;
+	}
+	/**
+	 * Displays the widget settings controls on the widget panel.
+	 * Make use of the get_field_id() and get_field_name() function * when creating your form elements. This handles the confusing stuff.
+	 */
+	function form( $instance ) {
+		/* Set up some default widget settings. */
+		$defaults = array( 
+			'title' => __('Great Deals', 'car-demon'),
+			'cars' => __('3', 'car-demon'),
+			 );
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+		<!-- Widget Title: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'car-demon'); ?></label>
+			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+			<label for="<?php echo $this->get_field_id( 'cars' ); ?>"><?php _e('# of Cars:', 'car-demon'); ?></label>
+			<input id="<?php echo $this->get_field_id( 'cars' ); ?>" name="<?php echo $this->get_field_name( 'cars' ); ?>" value="<?php echo $instance['cars']; ?>" style="width:100%;" />
+		</p>
+	<?php
+	}
+}
+
+function count_all_cars() {
+	global $wpdb;
+	$prefix = $wpdb->prefix;
+	$str_sql = "
+		SELECT Count(wposts.ID) AS cnt
+		FROM ".$prefix."posts wposts, ".$prefix."postmeta wpostmeta
+		WHERE wposts.ID = wpostmeta.post_id
+		AND post_type='cars_for_sale'
+		AND post_status='publish'
+		AND wpostmeta.meta_key = 'sold'
+		AND wpostmeta.meta_value = 'no'";
+	$the_count = $wpdb->get_results($str_sql);
+	if ($the_count) {
+		foreach ($the_count as $rec) {
+			$count = $rec->cnt;
+		}
+	}
+	return $count;
+}
+
+function car_demon_display_random_cars($stop) {
+	$stop = $stop + 1;
+	global $wpdb;
+	$car_demon_pluginpath = str_replace(str_replace('\\', '/', ABSPATH), get_option('siteurl').'/', str_replace('\\', '/', dirname(__FILE__))).'/';
+	$car_demon_pluginpath = str_replace('widgets','',$car_demon_pluginpath);
+	$total_cars = count_all_cars();
+	$total_cars = $total_cars - 7;
+	$start_at = rand(1,$total_cars);
+	if ($start_at < 0) $start_at = 0;
+	$str_sql = "
+		SELECT wposts.ID
+		FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
+		WHERE wposts.ID = wpostmeta.post_id
+		AND post_type='cars_for_sale'
+		AND post_status='publish'
+		AND wpostmeta.meta_key = 'sold'
+		AND wpostmeta.meta_value = 'no'
+		ORDER BY ID LIMIT ".$start_at.", 7";
+	$the_lists = $wpdb->get_results($str_sql);
+	$car = '';
+	$car_num = 0;
+	$count_limit = 0;
+	$stop_at = 0;
+	foreach ($the_lists as $the_list) {
+		$count_limit = $count_limit+1;
+		$stop_at = $stop_at+1;
+		if ($stop_at<$stop) {
+			$post_id = $the_list->ID;
+			$vehicle_year = strip_tags(get_the_term_list( $post_id, 'vehicle_year', '','', '', '' ));
+			$vehicle_make = strip_tags(get_the_term_list( $post_id, 'vehicle_make', '','', '', '' ));
+			$vehicle_model = strip_tags(get_the_term_list( $post_id, 'vehicle_model', '','', '', '' ));
+			$vehicle_condition = strip_tags(get_the_term_list( $post_id, 'vehicle_condition', '','', '', '' ));
+			$title = $vehicle_year . ' ' . $vehicle_make . ' '. $vehicle_model;
+			$title = substr($title, 0, 24);
+			$stock_value = get_post_meta($post_id, "_stock_value", true);
+			$mileage_value = get_post_meta($post_id, "_mileage_value", true);
+			$detail_output = '<span class="random_title">'.$title.'</span><br />';
+			$detail_output .= '<span class="random_text">';
+				$detail_output .= 'Condition: '.$vehicle_condition.'<br />';			
+			$detail_output .= '</span>';
+			$detail_output .= '<span class="random_text">';
+				$detail_output .= 'Mileage: '.$mileage_value.'<br />';
+			$detail_output .= '</span>';
+			$detail_output .= '<span class="random_text">';
+				$detail_output .= 'Stock#: '.$stock_value;
+			$detail_output .= '</span>';
+			$link = get_permalink($post_id);
+			$img_output = "<img onclick='window.location=\"".$link."\";' title='Click for price on this ".$title."' onerror='ImgError(this, \"no_photo.gif\");' class='random_widget_image' width='180px' height='135px' src='";
+			$img_output .= wp_get_attachment_thumb_url( get_post_thumbnail_id( $post_id ) );
+			$img_output .= "' />";
+			$ribbon = 'ribbon-just-added';
+			$ribbon = 'ribbon-great-deal';
+			if ($vehicle_condition == 'New') {
+				$ribbon = 'ribbon-new';
+			}
+			else {
+				if ($mileage_value < 60000) { $ribbon = 'ribbon-low-miles';	}
+				$tmp_price = get_post_meta($post_id, "_price_value", true);
+				if ($tmp_price < 12000) { $ribbon = 'ribbon-low-price';	}
+			}
+			$car .= '
+				<div class="random">
+					<div class="random_img">
+						<img onclick="window.location=\''.$link.'\';" class="random_car_widget" src="'. $car_demon_pluginpath .'theme-files/images/'.$ribbon.'.png" width="76" height="76" alt="New Ribbon" id="ribbon">
+						<img onclick="window.location=\''.$link.'\';" class="random_car_lookup" src="'. $car_demon_pluginpath .'theme-files/images/look_close.png" width="188" height="143" alt="New Ribbon" id="look_close" class="look_close">
+						'.$img_output.'
+					</div>
+					<div class="random_description">
+						'.$detail_output.'
+					</div>
+				</div>';
+		}
+	}
+	return $car;
+}
+?>
